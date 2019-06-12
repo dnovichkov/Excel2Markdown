d6c0fef8d39c60cbc65f651eb7d7c82169bac8d8
dmitriy <dmitriy.novichkov@gmail.com>
2019-06-12 14:54:53 +0400

    Adding first files

---
+++ main.py
@@ -0,0 +1,10 @@
+from loguru import logger
+
+
+def __main():
+    # TODO: parse command line arguments here.
+    logger.info("It is CLI-program for extracting Excel-tables to md-files")
+
+
+if __name__ == "__main__":
+    __main()
