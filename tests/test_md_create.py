ded6cff1852c205afb8907bd7e4a70a1a160e13f
novichkov <novichkov@smartsolutions-123.ru>
2019-06-13 10:13:23 +0400

    Fix test for create table with headers only

--- tests/test_md_create.py
+++ tests/test_md_create.py
@@ -6,6 +6,7 @@ class TestMarkdownCreate(unittest.TestCase):
 
     def test_headers_only(self):
         headers = ["Header1", "", "Header3"]
-        expected_table = "| Header1|| Column 3 |" \
+        expected_table = "| Header1||Header3|\r\n" \
                          "|-|-|-|"
-        self.assertEqual(headers, )
+        result = get_markdown_table(headers, None)
+        self.assertEqual(result, expected_table)
