f822140a5ee237df897f4342b4f3a6481fa6eafa
novichkov <novichkov@smartsolutions-123.ru>
2019-06-13 10:22:14 +0400

    Fix test for create table with headers only - removing wrong whitespace

--- tests/test_md_create.py
+++ tests/test_md_create.py
@@ -6,7 +6,7 @@ class TestMarkdownCreate(unittest.TestCase):
 
     def test_headers_only(self):
         headers = ["Header1", "", "Header3"]
-        expected_table = "| Header1||Header3|\r\n" \
+        expected_table = "|Header1||Header3|\r\n" \
                          "|-|-|-|"
         result = get_markdown_table(headers, None)
         self.assertEqual(result, expected_table)
